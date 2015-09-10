bb748a5aab4e3e61bf23f8364b33ed2357edab84
Amanda <a.vacharat@gmail.com>
2015-09-10 09:14:03 -0400

    fixed issues with merging, other stuff too

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ $("#form").hide();
 
 
 
-$(".seat").on('click', function(){
+$(".available").on('click', function(){
+	$(this).removeClass('available').addClass('selected');
 	$("form").slideDown( "slow");
 })
 
+$(".available").hover(function(){
+    $(this).fadeTo("slow", 0.25);
+    }, function(){
+    $(this).fadeTo("slow", 1);
+});
 
 
 });
