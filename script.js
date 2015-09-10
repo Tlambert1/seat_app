5be7973abec965d2a04fed382096819e19129c46
Troy <lambe1tm@gmail.com>
2015-09-10 10:12:25 -0400

    whatever

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-
+$(document).ready(function()
+{
+  
+  $('#wrapper').hover(function()
+  {
+    $(this).fadeTo(100, 0.25); 
+  }, function(){
+    $(this).fadeTo(100, 1);    
+  });
+  
+ 
+});
 
 
